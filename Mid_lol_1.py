b24818d3b64b4dff935d91d338c645e7be8c54ae
Kyungbaee <105343823+Kyungbaee@users.noreply.github.com>
2022-06-11 18:57:03 +0900

    Update Mid_lol_1.py

--- Mid_lol_1.py
+++ Mid_lol_1.py
@@ -1,19 +1,21 @@
+## 필요한 라이브러리 호출
 import pandas as pd
 import plotly.express as px
 import requests
 import time
 from tqdm import tqdm
 
+## 길어서 안보이는 컬럼 표시
 pd.options.display.max_columns = None
 
+## 밀리 세컨드(ms) -> 분(m) 초(s)
 def msTOm(ms):
     m = ms // 60000 
     s = (ms - m * 60000) // 1000
     return(f'{m}분 {s}초')
    
-   
+## 챌린저 5000게임 불러오고 결측치 제거 
 df = pd.read_parquet("data/df.match.gzip")
-dragon_4 = df[df["dragon_kill"] >= 4].sort_values("dragon_kill",ascending=False)["game_id"]
 df = df.reset_index(drop=True).drop(index=[128,129,3490,4855])
 df = df.reset_index(drop=True)
 out = []
@@ -22,15 +24,18 @@ for i in range(0,len(df)-1,2):
         out.append(i)
         out.append(i+1)
 df = df.drop(index=out).reset_index(drop=True)
+
+## 필요한 컬럼 추출
 df_2 = df[["game_id","team","win","dragon_first","dragon_kill"
     ,"riftHerald_first","riftHerald_kill","baron_first","baron_kill","gold_spent"]]
 
-dragon_count = 0
-blue_dragon_count = 0
-red_dragon_count = 0
-herald_count = 0
-blue_herald_count = 0
-red_herald_count = 0
+
+dragon_count = 0        ## 첫 용 처치 판수 총합
+blue_dragon_count = 0   ## 블루 진영 첫 용 처치 판수
+red_dragon_count = 0    ## 레드 진영 첫 용 처치 판수
+herald_count = 0        ## 첫 전령 처치 판수 총합
+blue_herald_count = 0   ## 블루 진영 첫 전령 처치 판수
+red_herald_count = 0    ## 레드 진영 첫 전령 처치 판수
 dragon_gold_diff = 0
 herald_gold_diff = 0
 except_count = 0
@@ -70,6 +75,7 @@ gold_diff_1 = round(100-dragon_gold_diff/dragon_count,2)
 dragon_red = round(red_dragon_count / herald_count * 100,2)
 herald_blue = round(blue_herald_count / herald_count * 100,2)
 gold_diff_2 = round(100-herald_gold_diff/herald_count,2)
+
 data = pd.DataFrame(columns=["Team Color","first_dragon","first_herald"])
 data.loc[0] = ["Blue Win Rate",dragon_blue,herald_blue]
 data.loc[1] = ["Red Win Rate",herald_red,dragon_red]
